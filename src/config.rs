d116c65317f7411619f7df6c81b88e65b6104569
cyteon <cyteon@hackclub.app>
2025-11-07 20:11:57 +0100

    config loading

---
+++ src/config.rs
@@ -0,0 +1,72 @@
+use std::process::exit;
+
+use serde::{Deserialize};
+use fancy_log::{LogLevel, log};
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    host: String,
+    port: u16,
+
+    server_name: String,
+    motd: String,
+    max_players: u32,
+}
+
+pub fn load_config() -> ServerConfig {
+    log(LogLevel::Info, "Loading config");
+
+    let path = "config.toml";
+    let file_exists = std::path::Path::new(path).exists();
+
+    if file_exists {
+        let config_str = std::fs::read_to_string(path);
+
+        if config_str.is_err() {
+            log(LogLevel::Error, "Failed to read config");
+            log(LogLevel::Error, "Stopping server");
+
+            exit(1);
+        }
+
+        match toml::from_str::<ServerConfig>(&config_str.unwrap()) {
+            Ok(config) => config,
+            Err(_) => {
+                log(LogLevel::Error, "Failed to parse config");
+                log(LogLevel::Error, "Stopping server");
+
+                exit(1);
+            }
+        }
+    } else {
+        let default_config = r#"# the host the server will bind to
+host = "0.0.0.0"
+
+# the port the server will listen on
+port = 25565
+
+# server details
+server_name = "An Mist Server"
+motd = "Welcome to the Mist Server!"
+max_players = 10"#;
+        
+        if std::fs::write(path, default_config).is_err() {
+            log(LogLevel::Error, "Failed to write default config");
+            log(LogLevel::Error, "Stopping server");
+
+            exit(1);
+        } else {
+            log(LogLevel::Info, "Default config created");
+        }
+
+        match toml::from_str::<ServerConfig>(default_config) {
+            Ok(config) => config,
+            Err(_) => {
+                log(LogLevel::Error, "Failed to parse default config");
+                log(LogLevel::Error, "Stopping server");
+
+                exit(1);
+            }
+        }
+    }
+}
