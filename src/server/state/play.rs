1a7c6ca63e67e5ea7338d49f0bdb1834da60419a
Cyteon <cyteon@hackclub.app>
2025-11-21 13:54:35 +0100

    idk atp

--- src/server/state/play.rs
+++ src/server/state/play.rs
@@ -1,7 +1,8 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 use fancy_log::{LogLevel, log};
-use tokio::{io::AsyncWriteExt, net::TcpStream, time::{self, timeout}};
+use once_cell::sync::Lazy;
+use tokio::{io::AsyncWriteExt, net::TcpStream, sync::RwLock, time::{self, timeout}};
 
 use crate::{
     net::{packet::{
@@ -15,10 +16,14 @@ use crate::{
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+pub static PLAYERS: Lazy<RwLock<HashMap<String, Arc<Mutex<Player>>>>> =
+    Lazy::new(|| RwLock::new(HashMap::new()));
+
 pub async fn play(socket: EncryptedStream<TcpStream>, mut player: Player) -> anyhow::Result<()> {
     log(LogLevel::Debug, format!("{} has entered the play state", player.name).as_str());
 
     let socket = Arc::new(Mutex::new(socket));
+    let player = Arc::new(Mutex::new(player));
 
     let keep_alive_future ={
         let socket = Arc::clone(&socket);
@@ -35,21 +40,50 @@ pub async fn play(socket: EncryptedStream<TcpStream>, mut player: Player) -> any
         })
     };
 
-    send_sync_player_position(&mut *socket.lock().await, &player).await?;
+    send_sync_player_position(&mut *socket.lock().await, &*player.lock().await).await?;
 
     log(
         LogLevel::Debug, 
-        format!("Sent initial player position to {}", player.name).as_str()
+        format!("Sent initial player position to {}", player.lock().await.name).as_str()
     );
 
     PLAYER_SOCKET_MAP.write().await.insert(
-        player.name.clone(), 
+        player.lock().await.name.clone(),
         Arc::clone(&socket)
+    );
+
+    PLAYERS.write().await.insert(
+        player.lock().await.name.clone(),
+        Arc::clone(&player)
     );
 
     for player_socket in PLAYER_SOCKET_MAP.read().await.values() {
         let mut socket_guard = player_socket.lock().await;
-        send_player_info_update(&mut *socket_guard, &player).await?;
+        let player_guard = player.lock().await;
+
+        send_player_info_update(&mut *socket_guard, vec![&*player_guard]).await?;
+    }
+
+    {
+        let mut socket_guard = socket.lock().await;
+        let player_guard = player.lock().await;
+        
+        let players = PLAYERS.read().await;
+
+        let other_players_owned: Vec<Player> = players
+            .values()
+            .filter_map(|p| {
+                let p_guard = p.blocking_lock();
+                if p_guard.name != player_guard.name {
+                    Some(p_guard.clone())
+                } else {
+                    None
+                }
+            })
+            .collect();
+        
+        let other_player_refs: Vec<&Player> = other_players_owned.iter().collect();
+        send_player_info_update(&mut *socket_guard, other_player_refs).await?;
     }
 
     loop {
@@ -59,7 +93,7 @@ pub async fn play(socket: EncryptedStream<TcpStream>, mut player: Player) -> any
             Ok(Ok(Some(packet))) => {
                 match packet {
                     ClientPacket::ConfirmTeleprortion(mut cursor) => {
-                        read_confirm_teleportation(&mut cursor, &mut player).await?;
+                        read_confirm_teleportation(&mut cursor, &mut *player.lock().await).await?;
                     }
 
                     _ => { }
@@ -71,7 +105,7 @@ pub async fn play(socket: EncryptedStream<TcpStream>, mut player: Player) -> any
             Err(e) => { 
                 log(
                     LogLevel::Error, 
-                    format!("{} has timed out during play state: {}", player.name, e).as_str()
+                    format!("{} has timed out during play state: {}", player.lock().await.name, e).as_str()
                 );
 
                 socket_guard.shutdown().await?;
@@ -82,7 +116,7 @@ pub async fn play(socket: EncryptedStream<TcpStream>, mut player: Player) -> any
             Ok(Err(e)) => {
                 log(
                     LogLevel::Error, 
-                    format!("Error while reading packet from {} during play state: {}", player.name, e).as_str()
+                    format!("Error while reading packet from {} during play state: {}", player.lock().await.name, e).as_str()
                 );
 
                 socket_guard.shutdown().await?;
