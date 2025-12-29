3432e1268c586a328035d7fde1effd73ba73fda7
cyteon <me@cyteon.dev>
2025-12-29 22:13:57 +0100

    add player info remove uuids

--- src/net/packets/clientbound/player_info_remove.rs
+++ src/net/packets/clientbound/player_info_remove.rs
@@ -1,9 +1,14 @@
 use crate::net::codec::write_var;
 
-pub async fn send_player_info_remove<W: tokio::io::AsyncWriteExt + Unpin>(stream: &mut W) -> anyhow::Result<()> {
+pub async fn send_player_info_remove<W: tokio::io::AsyncWriteExt + Unpin>(stream: &mut W, uuids: &[String]) -> anyhow::Result<()> {
     let mut packet_data = vec![0x43];
 
-    // todo
+    write_var(&mut packet_data, uuids.len() as i32)?;
+    for uuid in uuids {
+        let uuid_clean = uuid.replace("-", "");
+        let uuid_vec = hex::decode(uuid_clean)?;
+        packet_data.extend_from_slice(&uuid_vec);
+    }
 
     let mut len_prefix = Vec::with_capacity(5);
     write_var(&mut len_prefix, packet_data.len() as i32)?;
