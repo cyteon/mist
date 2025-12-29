156d6e3d675122ae5a832a32a31c7bc732908d7b
cyteon <me@cyteon.dev>
2025-12-29 13:44:48 +0100

    demo

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,7 +1,9 @@
 <div class="flex flex-col items-center min-h-screen">
     <h1 class="text-4xl mt-8 font-bold">Mist demo</h1>
-    <p class="mt-4 text-lg">You can view a demo video 
-        <a href="https://example.com" class="text-green-300 underline">here</a> or connect to 45.8.201.37:25565 with minecraft 1.21.10.
+    <p class="mt-4 text-lg">
+        You can view a demo video 
+        <a href="https://youtu.be/4ie10wYYcss" class="text-green-300 underline">here</a> 
+        or connect to 45.8.201.37:25565 with minecraft 1.21.10.
     </p>
     <p>If you get an error trying to connect, make sure you are on vanilla 1.12.10 and try to connect a couple more times</p>
 </div>
