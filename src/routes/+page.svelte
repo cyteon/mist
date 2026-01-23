bc0c768cd6acb296b5b96dcd9a5b002ca46e0e48
Cyteon <cyteon@hackclub.app>
2026-01-23 09:30:01 +0100

    create homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen w-full">
+    <nav class="absolute top-0 left-1/2 -translate-x-1/2 mt-4 space-x-2 flex">
+        <a href="https://github.com/cyteon/mist" target="_blank" rel="noopener" class="text-neutral-400 hover:text-green-300">source code</a>
+        <p class="text-neutral-500">\</p>
+        <a href="docs/introduction" target="_blank" rel="noopener" class="text-neutral-400 hover:text-green-300">docs</a>
+    </nav>
+
+    <div class="m-auto text-center">
+        <h1 class="text-7xl font-bold text-green-300">Mist</h1>
+        <h2 class="text-lg">An minecraft server implementation made from scratch in rust</h2>
+    </div>
+</div>
--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,5 +1,6 @@
 <div class="flex flex-col items-center min-h-screen">
     <h1 class="text-4xl mt-8 font-bold">Mist demo</h1>
+    <a href="/" class="text-green-300 underline">View site</a>
     <p class="mt-4 text-lg">
         You can view a demo video 
         <a href="https://youtu.be/4ie10wYYcss" class="text-green-300 underline">here</a> 
