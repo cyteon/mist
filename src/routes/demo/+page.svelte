3f4491183d0134fdc0761d816e446960e5f8d735
Cyteon <129582290+cyteon@users.noreply.github.com>
2026-01-25 11:34:46 +0100

    aaa

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -2,7 +2,7 @@
     <h1 class="text-4xl mt-8 font-bold">Mist demo</h1>
     <p class="mt-4 text-lg">
         You can view a demo video 
-        <a href="https://youtu.be/mOHG03tqhy4" class="text-green-300 underline">here</a> 
+        <a href="https://youtu.be/Z4eodeBYWtA" class="text-green-300 underline">here</a> 
         and view the site <a href="/" class="text-green-300 underline">here</a>
     </p>
 </div>
