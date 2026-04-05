286c9b330d3c3a8fce6cc59da669682f4398e7d0
cyteon <me@cyteon.dev>
2026-04-05 11:43:14 +0200

    give player crafting table contents on close

--- src/net/packets/serverbound/container_close.rs
+++ src/net/packets/serverbound/container_close.rs
@@ -7,6 +7,23 @@ pub async fn read_container_close<R: AsyncReadExt + Unpin>(
     player: &mut Player,
 ) -> anyhow::Result<()> {
     let _window_id = read_var(stream).await?;
+
+    match player.current_window {
+        Some(window) => match window {
+            crate::types::player::WindowType::CraftingTable(grid) => {
+                for i in 1..=9 {
+                    if let Some(item_stack) = grid[i] {
+                        player
+                            .give_item(item_stack.item_id, item_stack.count as i32)
+                            .await?;
+                    }
+                }
+            }
+        },
+
+        None => {}
+    }
+
     player.current_window = None;
 
     Ok(())
